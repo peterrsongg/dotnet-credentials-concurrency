ec4e4e2c3316f9d52107af83bf62a35c49b3f473
Peter Song <petesong@amazon.com>
2026-03-30 12:20:15 -0700

    Add region

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@ using Amazon.S3.Model;
 using AWS.Logger;
 using AWS.Logger.SeriLog;
 using Serilog;
-using var s3Client = new AmazonS3Client();
+using var s3Client = new AmazonS3Client(RegionEndpoint.USWest2);
 var bucketName = "dotnet-8554";
 Log.Logger = new LoggerConfiguration()
     .WriteTo.AWSSeriLog(new AWSLoggerConfig(bucketName))
